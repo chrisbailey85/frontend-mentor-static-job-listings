6f153682070cca64c2f9ed2dac04a7e2b341a3ff
Chris Bailey <chris85bailey@aol.co.uk>
2021-06-19 11:22:41 +0100

    add code to remove hidden class when clear btn is clicked, and remove a comment that is no longer needed

--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,6 @@ let selectedTags = document.querySelectorAll('.selected__tag');
 
 tagBtns.forEach(btn => {
   btn.addEventListener('click', (e) => {
-    let tag = document.querySelectorAll('.tag')
     const clear = document.querySelector('.selected__clear');
     const data = e.target.dataset.tag;
     selectedContainer.classList.remove('hidden');
@@ -19,13 +18,7 @@ tagBtns.forEach(btn => {
 
   })
 })
-// <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
-//         <path
-//           fill="#FFF"
-//           fill-rule="evenodd"
-//           d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"
-//         />
-//       </svg>
+
 function selectedTag(data) {
   let tag = document.createElement('div');
   tag.classList.add('selected__tag');
@@ -72,7 +65,8 @@ function addedClearBtn() {
   const clear = document.querySelector('.selected__clear');
 
   clear.addEventListener('click', () => {
-    selectedContainer.innerHTML = ``
+    cards.forEach(card => card.classList.remove('hidden'));
+    selectedContainer.innerHTML = ``;
     selectedContainer.classList.add('hidden');
   })
 }
